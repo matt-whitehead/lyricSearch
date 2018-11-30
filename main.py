bc5a7d2af2498f150026c6c9cc237513a0036ab0
Matt Whitehead <mwwhite@umich.edu>
2018-11-30 14:44:11 -0500

    fix lyrics formatting

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from whoosh.index import open_dir
 from whoosh.qparser import QueryParser
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, Markup
 
 # read in the index we created
 ix = open_dir('index')
@@ -20,7 +20,7 @@ def generate_result(query):
         for i in results:
             title.append(i['title'])
             artist.append(i['artist'])
-            lyrics.append(i['lyrics'])
+            lyrics.append(Markup(i['lyrics'].replace('\n', '<br />')))
 
         return title, artist, lyrics
 
